67d8513b01347d78f35634f72ca7afd01a7a1bb5
Florentino Salazar <florentini.salaz@gmail.com>
2018-09-13 01:14:49 -0500

    incomplete modification

---
+++ src/main/java/io/vertx/guides/wiki/HttpServerVerticle.java
@@ -0,0 +1,40 @@
+package io.vertx.guides.wiki;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import io.vertx.core.AbstractVerticle;
+import io.vertx.core.Future;
+import io.vertx.core.http.HttpServer;
+import io.vertx.ext.web.Router;
+import io.vertx.ext.web.handler.BodyHandler;
+
+public class HttpServerVerticle extends AbstractVerticle {
+	private static final Logger LOGGER = LoggerFactory.getLogger(HttpServerVerticle.class);
+	public static final String CONFIG_HTTP_SERVER_PORT = "http.server.port";
+	public static final String CONFIG_WIKIDB_QUEUE = "wikidb.queue";
+	private String wikiDbQueue = "wikidb.queue";
+
+	@Override
+	public void start(Future<Void> startFuture) throws Exception {
+		wikiDbQueue = config().getString(CONFIG_WIKIDB_QUEUE, "wikidb.queue");
+		HttpServer server = vertx.createHttpServer();
+		Router router = Router.router(vertx);
+		router.get("/").handler(this::indexHandler);
+		router.get("/wiki/:page").handler(this::pageRenderingHandler);
+		router.post().handler(BodyHandler.create());
+		router.post("/save").handler(this::pageUpdateHandler);
+		router.post("/create").handler(this::pageCreateHandler);
+		router.post("/delete").handler(this::pageDeletionHandler);
+		int portNumber = config().getInteger(CONFIG_HTTP_SERVER_PORT, 8080);
+		server.requestHandler(router::accept).listen(portNumber, ar -> {
+			if (ar.succeeded()) {
+				LOGGER.info("HTTP server running on port " + portNumber);
+				startFuture.complete();
+			} else {
+				LOGGER.error("Could not start a HTTP server", ar.cause());
+				startFuture.fail(ar.cause());
+			}
+		});
+	}
+}
